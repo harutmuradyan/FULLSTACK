5835e1e0f2d5e1b1db3e29db02685ade1db615e7
Harut <harut7159@gmail.com>
2023-01-28 22:07:40 +0400

    Create-react-app

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const products = require("./db/db");
+
+const app = express();
+
+app.get('/products', function (req, res) {
+    res.json(products);
+  })
+
+console.log("Server running 5000")
+
+
+app.listen(5000);
